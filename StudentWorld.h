2fa5faa9a9ef4f9b8445a54709dea5ae2263a1cd
SnowWalk-Alpha <60299660+SnowWalk-Alpha@users.noreply.github.com>
2021-02-17 21:09:06 +0800

    Create StudentWorld.h

---
+++ StudentWorld.h
@@ -0,0 +1,28 @@
+#ifndef STUDENTWORLD_H_
+#define STUDENTWORLD_H_
+
+#include "GameWorld.h"
+#include <string>
+#include "Actor.h"
+#include <map>
+#include <vector>
+#include <list>
+using namespace std;
+// Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
+
+class StudentWorld : public GameWorld
+{
+public:
+    StudentWorld(std::string assetPath);
+    ~StudentWorld();
+    virtual int init();
+    virtual int move();
+    virtual void cleanUp();
+    void setupBorderline();
+private:
+    list<Actor*> actors;
+    GhostRacer* theGR;
+    double lastWhiteY;
+};
+
+#endif // STUDENTWORLD_H_
