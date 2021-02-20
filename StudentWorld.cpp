829de4a400f22c0718997ccbc245a9947880a348
SnowWalk-Alpha <60299660+SnowWalk-Alpha@users.noreply.github.com>
2021-02-20 21:58:56 +0800

    Update StudentWorld.cpp

--- StudentWorld.cpp
+++ StudentWorld.cpp
@@ -101,6 +101,33 @@ void StudentWorld::addHolyWaterGoodies()
     }
 }
 
+void StudentWorld::addHealingGoodies(Actor* theActor)
+{
+    int ChanceOfHealingGoodies = randInt(1, 5);
+    if(ChanceOfHealingGoodies==1)
+    {
+        actors.push_back(new Healing_Goodies(theActor->getX(),theActor->getY(),this));
+    }
+}
+
+void StudentWorld::addLostSouls()
+{
+    int LEFT_EDGE = ROAD_CENTER - ROAD_WIDTH/2;
+    int RIGHT_EDGE = ROAD_CENTER + ROAD_WIDTH/2;
+    int ChanceOfLostSoul = 100;
+    if(randInt(0, ChanceOfLostSoul-1)==0)
+    {
+        actors.push_back(new LostSouls(randInt(LEFT_EDGE, RIGHT_EDGE),VIEW_HEIGHT,this));
+    }
+}
+
+void StudentWorld::addZombieCab()
+{
+    int ChanceVehicle=max(100-getLevel()*10,20);
+    if(randInt(0, ChanceVehicle-1)==0)
+        MovementPlan();
+}
+
 void StudentWorld::AlldoSomething()
 {
     list<Actor*>::iterator it=actors.begin();
@@ -156,13 +183,57 @@ bool StudentWorld::checkOverlapofHoly(Actor* theActor)
     return false;
 }
 
+bool StudentWorld::checkZombieCabFront()
+{
+    list<Actor*>:: iterator it=actors.begin();
+    list<Actor*>:: iterator it2=actors.begin();
+    while(it!=actors.end())
+    {
+        if((*it)->isZombieCab())
+        {
+            if(theGR->getX()==(*it)->getX() && theGR->getY()>(*it)->getY() && theGR->getY()-(*it)->getY()<96)
+                return true;
+            
+            while(it2!=actors.end())
+            {
+                if((*it2)->getX()==(*it)->getX() && (*it2)->getY()>(*it)->getY() && ((*it2)->getY()-(*it)->getY()<96) && (*it2)->isCollisionAvoidance())
+                    return true;
+                it2++;
+            }
+        }
+        it++;
+    }
+    return false;
+}
+
+bool StudentWorld::checkZombieCabBehind()
+{
+    list<Actor*>:: iterator it=actors.begin();
+    while(it!=actors.end())
+    {
+        if((*it)->isZombieCab())
+        {
+            if(theGR->getX()==(*it)->getX() && theGR->getY()<(*it)->getY() && (*it)->getY()-theGR->getY()<96)
+                return true;
+            list<Actor*>:: iterator it2=actors.begin();
+            while(it2!=actors.end())
+            {
+                if((*it2)->getX()==(*it)->getX() && (*it2)->getY()<(*it)->getY() && (*it)->getY()-(*it2)->getY()<96)
+                    return true;
+                it2++;
+            }
+        }
+        it++;
+    }
+    return false;
+}
+
 int StudentWorld::init()
 {
     lastWhiteY=224;
     theGR= new GhostRacer(128,32,this);
     
     setupBorderline();
-    
     
     return GWSTATUS_CONTINUE_GAME;
 }
@@ -180,8 +251,10 @@ int StudentWorld::move()
     AlldoSomething();
     RemoveDead();
     addnewBorderline();
-    addnewOilSlicks();
-    addHolyWaterGoodies();
+   // addnewOilSlicks();
+  //  addHolyWaterGoodies();
+   // addLostSouls();
+    addZombieCab();
     return GWSTATUS_CONTINUE_GAME;
 }
 
@@ -196,4 +269,135 @@ void StudentWorld::cleanUp()
             it++;
             actors.erase(toErase);
     }
+}
+
+
+void StudentWorld::MovementPlan() //Need Double Check
+{
+    int cur_lane=0;
+    int startY, startSpeed;
+    int LEFT_EDGE = ROAD_CENTER - ROAD_WIDTH/2;
+    int RIGHT_EDGE = ROAD_CENTER + ROAD_WIDTH/2;
+    list<Actor*>::iterator Iterator = actors.begin();
+        //LEFT LANE
+        while(Iterator!=actors.end())
+        {
+            if((*Iterator)->getX()>=LEFT_EDGE && (*Iterator)->getX()<LEFT_EDGE+ROAD_WIDTH/3)
+            {
+                if(closestBOT())
+                {
+                    startY=SPRITE_HEIGHT/2;
+                    startSpeed=theGR->getVertSpeed()+randInt(2, 4);
+                    actors.push_back(new ZombieCab(ROAD_CENTER-ROAD_WIDTH/3,startY,this, startSpeed));
+                    cur_lane=1;
+                    break;
+                }
+                if(closestTOP())
+                {
+                    startY=VIEW_HEIGHT-SPRITE_HEIGHT/2;
+                    startSpeed=theGR->getVertSpeed()-randInt(2, 4);
+                    actors.push_back(new ZombieCab(ROAD_CENTER-ROAD_WIDTH/3,startY,this, startSpeed));
+                    cur_lane=1;
+                    break;
+                }
+            }
+            Iterator++;
+        }
+        //MIDDLE LANE
+        while(Iterator!=actors.end())
+        {
+            if((*Iterator)->getX()>=LEFT_EDGE+ROAD_WIDTH/3 && (*Iterator)->getX()<RIGHT_EDGE-ROAD_WIDTH/3)
+            {
+                if(closestBOT() )
+                {
+                    startY=SPRITE_HEIGHT/2;
+                    startSpeed=theGR->getVertSpeed()+randInt(2, 4);
+                    actors.push_back(new ZombieCab(ROAD_CENTER,startY,this, startSpeed));
+                    cur_lane=2;
+                    break;
+                }
+                if(closestTOP())
+                {
+                    startY=VIEW_HEIGHT-SPRITE_HEIGHT/2;
+                    startSpeed=theGR->getVertSpeed()-randInt(2, 4);
+                    actors.push_back(new ZombieCab(ROAD_CENTER,startY,this, startSpeed));
+                    cur_lane=2;
+                    break;
+                }
+            }
+            Iterator++;
+        }
+        //RIGHT LANE
+        while(Iterator!=actors.end())
+        {
+            if((*Iterator)->getX()>=RIGHT_EDGE-ROAD_WIDTH/3 && (*Iterator)->getX()<RIGHT_EDGE)
+            {
+                if(closestBOT())
+                {
+                    startY=SPRITE_HEIGHT/2;
+                    startSpeed=theGR->getVertSpeed()+randInt(2, 4);
+                    actors.push_back(new ZombieCab(ROAD_CENTER+ROAD_WIDTH/3,startY,this, startSpeed));
+                    cur_lane=3;
+                    break;
+                }
+                if(closestTOP())
+                {
+                    startY=VIEW_HEIGHT-SPRITE_HEIGHT/2;
+                    startSpeed=theGR->getVertSpeed()-randInt(2, 4);
+                    actors.push_back(new ZombieCab(ROAD_CENTER+ROAD_WIDTH/3,startY,this, startSpeed));
+                    cur_lane=3;
+                    break;
+                }
+            }
+            Iterator++;
+        }
+        
+    if(cur_lane==0) //No viable lane identified as the "chosen lane"
+        return;
+   
+}
+
+
+bool StudentWorld::closestBOT()
+{
+    list<Actor*>::iterator it = actors.begin();
+    int min=260;
+    Actor* closestActor=(*it);
+    while(it!=actors.end())
+    {
+        if((*it)->isCollisionAvoidance())
+        {
+            if((*it)->getY()<min)
+            {
+                closestActor=(*it);
+                min=(*it)->getY();
+            }
+        }
+        it++;
+    }
+    if((closestActor->getY()>(VIEW_HEIGHT/3) && min!=260) || min==260)//min doesn't change, meaning no such actor; or the min changes and meets the condition
+        return true;
+    return false;
+}
+
+bool StudentWorld::closestTOP()
+{
+    list<Actor*>::iterator it2 = actors.begin();
+    int min2=-1;
+    Actor* closestActor2=(*it2);
+    while(it2!=actors.end())
+    {
+        if((*it2)->isCollisionAvoidance())
+        {
+            if((*it2)->getY()>min2)
+            {
+                closestActor2=(*it2);
+                min2=(*it2)->getY();
+            }
+        }
+        it2++;
+    }
+    if((closestActor2->getY()<(VIEW_HEIGHT*2/3) && min2!=-1) || min2==-1)
+        return true;
+    return false;
 }
